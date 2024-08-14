289293e8d3c96ea03dfb463050f82add2414ef25
Oscdivin <divineuwandu84@Gmail.com>
2024-08-14 18:25:50 +0100

    Header Update

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,14 +1,57 @@
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa"
 
 
 const Footer = () => {
   return (
-    <footer className="bg-veryDarkBlue">
+    <footer className="bg-[#000]">
       <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
-        <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
+        <div className="flex flex-col-reverse items-center justify-between space-y-12 gap-3 md:flex-col md:space-y-0 md:items-start">
           <div>
             <img src="https://t4.ftcdn.net/jpg/02/89/30/25/360_F_289302587_UrGkCdrmzoZdRqgK5JkFI9BitPrnDGjX.jpg" alt="" className="w-20 h-20 rounded-full object-cover"/>
           </div>
+          <div className="flex justify-center space-x-4">
+        <a href="">   <FaFacebook size={20} className="text-brightRedLight"/></a>
+        <a href="https://www.linkedin.com/in/divineuwandu/">
+           <FaLinkedin size={20} className="text-brightRedLight"/>
+
+        </a>
+        <a href="https://github.com/DevDivine-001">
+           <FaGithub size={20} className="text-brightRedLight"/>
+
+        </a>
+          </div>
         </div>
+        <div className="flex justify-around space-x-32">
+            <div className="flex flex-col space-y-3 text-white">
+                  <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Pricing</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Product</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">About Us</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Careers</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Community</a>
+            </div>
+
+            <div className="flex flex-col space-y-3 text-white">
+                  <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Careerrs</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Community</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Privacy & Policy</a>
+                
+            </div>
+            
+            </div>
+            <div className="flex flex-col justify-between">
+              <form action=""> bg-brightRedLight hover:bg-brightRedLight focus:outline-none
+                <div className="flex space-x-3">
+                  <input type="text" className="flex-1 px-4 rounded-full focus:outline-none "
+                  placeholder="Updated in your inbox"/>
+                  <button className="px-6 py-2 text-[#fff] rounded-full bg-brightRedLight hover:bg-brightRedLight focus:outline-none">Go</button>
+
+                </div>
+              </form>
+              <div className="hidden text-[#fff] md:block">
+                Copyright & Copy; 2024, All Right Reserved
+
+              </div>
+            </div>
 
       </div>
     </footer>
--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai"
 
 
 const Header = () => {
+  const [DropDownMenu, setDropDownMenu] = useState(false)
+  const handleNav = () =>{
+    setDropDownMenu(!DropDownMenu)
+  }
   return (
     <header className="top-0 sticky bg-[#fff] shadow-md">
        <nav  className="container mx-auto p-6">
@@ -18,6 +24,27 @@ const Header = () => {
                 {/* <a href=""></a> */}
             </div>
             <a href="" className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight hidden md:block  justify-center items-center">Get Started</a>
+
+        {/* <button className=" block hamburger md:hidden focus:outline-none" onClick={handleNav}>
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button> */}
+         <div onClick={handleNav} className="cursor-pointer block md:hidden text-[#000]">
+       {!DropDownMenu ?  <AiOutlineMenu size={20}/> : <AiOutlineClose size={20}/>}</div>
+
+        <div       className={!DropDownMenu ? "fixed left-[-100%] ease-in-out duration-700 bg-[#fff]" : "fixed left-0 top-[70px] w-[100%] h-full border-r-none ease-in-out duration-700 border-none cursor-pointer block md:hidden bg-[#fff]"}onClick={handleNav}>
+          <div className="absolute flex flex-col items-center  self-end  py-8 mt-10 space-y-6 font-bold  sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
+              <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Pricing</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Product</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">About Us</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Careers</a>
+                <a href="" className="hover:text-darkGrayIsBlue ease-in-out duration-500">Community</a>
+
+
+          </div>
+
+        </div>
 
         </nav>
        </nav>
