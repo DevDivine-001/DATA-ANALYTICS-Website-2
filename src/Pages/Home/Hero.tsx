fc0d437a86231abd01f63de5d80fa26dc13d6cce
Oscdivin <divineuwandu84@Gmail.com>
2024-08-14 09:23:05 +0100

    Here Pages is   is done now

--- src/Pages/Home/Hero.tsx
+++ src/Pages/Home/Hero.tsx
@@ -3,7 +3,7 @@ import img from "../../assets/react.svg"
 const Hero = () => {
   return (
     <section id="hero" className="flex justify-center ic">
-        {/* <div className="container flex flex-col reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row ">
+        <div className="container flex flex-col reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row ">
             <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
                 <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
                     Bring everyone together to build better products
@@ -20,7 +20,7 @@ const Hero = () => {
             <div className="md:w-3/5 max-md:w-full">
                 <img src={img} alt="" className="w-full object-cover " />
             </div>
-        </div> */}
+        </div>
     </section>
   )
 }
