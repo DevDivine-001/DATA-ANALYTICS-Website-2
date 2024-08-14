986a6c98d8adfee827f63b60ab2bd3af020ffb2c
Oscdivin <divineuwandu84@Gmail.com>
2024-08-14 09:13:14 +0100

    HeroPages is done

--- src/Pages/Home/Hero.tsx
+++ src/Pages/Home/Hero.tsx
@@ -1,28 +1,28 @@
-// import img from "../../assets/react.svg"
+import img from "../../assets/react.svg"
 
-// const Hero = () => {
-//   return (
-//     <section id="hero" className="flex justify-center items-center">
-//         <div className="container flex flex-col reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row ">
-//             <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
-//                 <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
-//                     Bring everyone together to build better products
-//                 </h1>
-//                 <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
-//                     Manage makes it simple for software teams to plan day-to-day tasks
-//                     While keeping the larger team goals in view.
-//                 </p>
-//                 <div className="flex justify-center md:justify-start">
-//                     <a href="#" className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight  md:block  justify-center items-center">Get Started</a>
+const Hero = () => {
+  return (
+    <section id="hero" className="flex justify-center ic">
+        {/* <div className="container flex flex-col reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row ">
+            <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
+                <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
+                    Bring everyone together to build better products
+                </h1>
+                <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
+                    Manage makes it simple for software teams to plan day-to-day tasks
+                    While keeping the larger team goals in view.
+                </p>
+                <div className="flex justify-center md:justify-start">
+                    <a href="#" className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight  md:block  justify-center items-center">Get Started</a>
 
-//                 </div>
-//             </div>
-//             <div className="md:w-3/5 max-md:w-full">
-//                 <img src={img} alt="" className="w-full object-cover " />
-//             </div>
-//         </div>
-//     </section>
-//   )
-// }
+                </div>
+            </div>
+            <div className="md:w-3/5 max-md:w-full">
+                <img src={img} alt="" className="w-full object-cover " />
+            </div>
+        </div> */}
+    </section>
+  )
+}
 
-// export default Hero
+export default Hero
